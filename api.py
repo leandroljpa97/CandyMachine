f1e593ad1cb4a4ac8bf9359a32e25c7b181d3306
leandroljpa97 <leandro.pereira@tecnico.ulisboa.pt>
2018-11-17 00:43:48 +0000

    cliente para testar os posts

--- api.py
+++ api.py
@@ -2,21 +2,18 @@ from flask import Flask
 from flask import render_template, jsonify
 from flask import request
 import requests
-import bookDB
-
-
-def get_func(idx):
-
-	r = requests.get("http://127.0.0.1:5000/questions"+idx)
-	print(r.status_code)
-	data = r.json()
-	print(data)
-
-#!/usr/bin/env python
 
 
 
-for i in range (1,5):
-	get_func(i)
 
+def login():
+	payload = {"name":"andre","course":"xixixixixixi","id":2022,"mail":"ahahah","time":60,"correct":3,"total":3}
+	r = requests.post("http://127.0.0.1:5000/PlayerInformation", json=payload)
+	print(r.text)
 
+def results():
+	payload = {"time":900,"correct":21,"total":30,"id":2022}
+	r = requests.post("http://127.0.0.1:5000/Results", json=payload)
+	print(r.text)
+
+results()
