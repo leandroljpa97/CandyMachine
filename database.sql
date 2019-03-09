04e617be15560551142957542b223b0b355fb46f
leandroljpa97 <leandro.pereira@tecnico.ulisboa.pt>
2019-03-09 15:03:34 +0000

    mudança no server e na BD

--- database.sql
+++ database.sql
@@ -4,12 +4,12 @@ drop table if exists highscore;
 create table quiz(
 id integer,
 question varchar(255),
-correct_answer varchar(255),
+answer1 varchar(255),
 answer2 varchar(255),
 answer3 varchar(255),
 answer4 varchar(255),
-primary key(id)
-);
+correct_answer integer,
+primary key(id));
 
 create table highscore(
 name varchar(255),
@@ -24,36 +24,36 @@ total_questions integer);
 
 
 
-insert into quiz values (1,'Quem foi o primeiro rei de Portugal?','D.Afonso Henriques','D. Manuel |',' D.João', 'D. Afonso |');
-insert into quiz values (2,'Qual instrução serve para fazer um ciclo','while','if',' case', 'print');
-insert into quiz values (3,'Qual instrução serve para fazer um ciclo','nenhuma das anteriores','if',' case', 'print');
-insert into quiz values (4,'Qual instrução serve para fazer um ciclo','for','if',' case', 'print');
-insert into quiz values(5,'Quem descobriu a india?','Vasco da Gama','D.Manuel I','Infante Dom Henriques', 'D.Pedro V');
-insert into quiz values(6,'No mandato de quem é que se descobriu a india?','D.Manuel I','Vasco da Gama','Infante Dom Henriques', 'D. Pedro V');
-insert into quiz values(7,'Para que serve o SSH','aceder remotamente a outro computador','ter seguro de que a mensagem chegou ao destino','fazer scripts','x');
-insert into quiz values (8,'Qual a fruta que atingiu Isaac Newtown na cabeça quando este criou a lei da gravitação universal?','Maça','Pêra','Laranja', 'Banana');
-insert into quiz values (9,'300000000 m/s é a velocidade de que partícula no vácuo?','Fotão','Protão','Eletrão', 'Neutrão');
-insert into quiz values (10,'Qual é a 1ª Lei de Newton?','Princípio da inércia','princípio da ação e reação','Lei fundamental da dinâmica', 'Relatividade Geral');
-insert into quiz values (11,'Qual é a 2ª Lei de Newton?',' Lei fundamental da dinâmica','Princípio da inércia','Relatividade Geral', 'Princípio da ação e reação');
-insert into quiz values (12,'Qual é a 3ª Lei de Newton?','Princípio da ação e reação','Lei fundamental da dinâmica','Relatividade Geral', 'Princípio da inércia');
-insert into quiz values (13,'Que físico desenvolveu a teoria da relatividade geral?','Albert Einstein','Isaac Newton','Niels Bohr', 'Joseph John Thomson');
-insert into quiz values (14,'O gato de Schrödinger está?','Nenhuma das anteriores','Morto', 'Vivo', 'A dormir');
-insert into quiz values (15,'Qual a força fundamental mais forte?','Força nuclear forte','Força nuclear fraca', 'Gravidade', 'Força Eletromagnética');
-insert into quiz values (16,'Qual a força fundamental mais fraca?','Gravidade','Força nuclear fraca','Força Eletromagnética', 'Força nuclear forte');
-insert into quiz values (17,'Qual é o único ramo da física que é indeterminista?','Mecânica quântica','Termodinâmica', 'Eletromagnetismo', 'Óptica e ondas');
-insert into quiz values (18,'Quem é o autor do livro "A Brief History of Time "?','Stephen Hawking','Peter Higgs','Isaac Newton', 'Walter Lewin');
-insert into quiz values (19,'Quem é o apresentador da série "Cosmos" apresentada em 1980?','Carl Sagan','Stephen Hawking', 'Neil deGrasse Tyson', 'Alan Guth');
-insert into quiz values (20,'Quem é o apresentador da série americana "Cosmos: A Spacetime Odyssey"?','Neil deGrasse Tyson','Carl Sagan','Alan Guth', 'Stephen Hawking');
-insert into quiz values (21,'Qual planeta do Sistema Solar tem os dias mais curtos?','Júpiter','Saturno','Marte','Mercúrio');
-insert into quiz values (22,'Que planetas do Sistema Solar são conhecidos por "gigantes gelados"?','Urano e Neptuno','Júpiter e Saturno','Terra e Marte', 'Mercúrio e Vénus');
-insert into quiz values (23,'Qual destes planetas é um gigante gasoso?','Saturno','Vénus','Marte', 'Mercúrio');
-insert into quiz values (24,'Qual destas empresas desenvolveu o "Falcon Heavy"?','SpaceX','Blue Origin','Virgin Galactic', 'Boeing');
-insert into quiz values (25,'Qual o nome do primeiro chimpanzé lançado para o espaço em 1961?','Ham','Albert II','Dezik', 'Hector');
-insert into quiz values (26,'Qual o nome do primeiro ser vivo a orbitar o planeta Terra?','Laika','Belka','Sally', 'Strelka');
-insert into quiz values (27,'Que planeta no Sistema Solar tem o eixo de rotação inclinado 98º?','Urano','Vénus','Júpiter', 'Saturno');
-insert into quiz values (28,'Mimas, Dione e Titã são luas de que planeta?','Saturno','Júpiter','Neptuno', 'Urano');
-insert into quiz values (29,'Ceres é um planeta anão, entre que planetas órbita?','Marte e Júpiter','Saturno e Urano','Proxima Centauri a e Proxima Centauri b', 'Júpiter e Saturno');
-insert into quiz values (30,'Quantos planetas no Sistema Solar têm luas?','6','7','4', '5');
+insert into quiz values (1,'Quem foi o primeiro rei de Portugal?','D.Afonso Henriques','D. Manuel |',' D.João', 'D. Afonso |',1);
+insert into quiz values (2,'Qual instrução serve para fazer um ciclo','if',' case','while', 'print',3);
+insert into quiz values (3,'Qual instrução serve para fazer um ciclo','if',' case', 'print','nenhuma das anteriores',4);
+insert into quiz values (4,'Qual instrução serve para fazer um ciclo','for','if',' case', 'print',1);
+insert into quiz values(5,'Quem descobriu a india?','D.Manuel I','Vasco da Gama','Infante Dom Henriques', 'D.Pedro V',2);
+insert into quiz values(6,'No mandato de quem é que se descobriu a india?','Vasco da Gama','D.Manuel I','Infante Dom Henriques', 'D. Pedro V',2);
+insert into quiz values(7,'Para que serve o SSH','ter seguro de que a mensagem chegou ao destino','aceder remotamente a outro computador','fazer scripts','fazer ciclos',2);
+insert into quiz values (8,'Qual a fruta que atingiu Isaac Newtown na cabeça quando este criou a lei da gravitação universal?','Pêra','Laranja', 'Maça','Banana',3);
+insert into quiz values (9,'300000000 m/s é a velocidade de que partícula no vácuo?','Fotão','Protão','Eletrão', 'Neutrão',1);
+insert into quiz values (10,'Qual é a 1ª Lei de Newton?','Princípio da inércia','princípio da ação e reação','Lei fundamental da dinâmica', 'Relatividade Geral',1);
+insert into quiz values (11,'Qual é a 2ª Lei de Newton?',' Lei fundamental da dinâmica','Princípio da inércia','Relatividade Geral', 'Princípio da ação e reação',1);
+insert into quiz values (12,'Qual é a 3ª Lei de Newton?','Princípio da ação e reação','Lei fundamental da dinâmica','Relatividade Geral', 'Princípio da inércia',1);
+insert into quiz values (13,'Que físico desenvolveu a teoria da relatividade geral?','Albert Einstein','Isaac Newton','Niels Bohr', 'Joseph John Thomson',1);
+insert into quiz values (14,'O gato de Schrödinger está?','Nenhuma das anteriores','Morto', 'Vivo', 'A dormir',1);
+insert into quiz values (15,'Qual a força fundamental mais forte?','Força nuclear forte','Força nuclear fraca', 'Gravidade', 'Força Eletromagnética',1);
+insert into quiz values (16,'Qual a força fundamental mais fraca?','Gravidade','Força nuclear fraca','Força Eletromagnética', 'Força nuclear forte',1);
+insert into quiz values (17,'Qual é o único ramo da física que é indeterminista?','Mecânica quântica','Termodinâmica', 'Eletromagnetismo', 'Óptica e ondas',1);
+insert into quiz values (18,'Quem é o autor do livro "A Brief History of Time "?','Stephen Hawking','Peter Higgs','Isaac Newton', 'Walter Lewin',1);
+insert into quiz values (19,'Quem é o apresentador da série "Cosmos" apresentada em 1980?','Carl Sagan','Stephen Hawking', 'Neil deGrasse Tyson', 'Alan Guth',1);
+insert into quiz values (20,'Quem é o apresentador da série americana "Cosmos: A Spacetime Odyssey"?','Neil deGrasse Tyson','Carl Sagan','Alan Guth', 'Stephen Hawking',1);
+insert into quiz values (21,'Qual planeta do Sistema Solar tem os dias mais curtos?','Júpiter','Saturno','Marte','Mercúrio',1);
+insert into quiz values (22,'Que planetas do Sistema Solar são conhecidos por "gigantes gelados"?','Urano e Neptuno','Júpiter e Saturno','Terra e Marte', 'Mercúrio e Vénus',1);
+insert into quiz values (23,'Qual destes planetas é um gigante gasoso?','Saturno','Vénus','Marte', 'Mercúrio',1);
+insert into quiz values (24,'Qual destas empresas desenvolveu o "Falcon Heavy"?','SpaceX','Blue Origin','Virgin Galactic', 'Boeing',1);
+insert into quiz values (25,'Qual o nome do primeiro chimpanzé lançado para o espaço em 1961?','Ham','Albert II','Dezik', 'Hector',1);
+insert into quiz values (26,'Qual o nome do primeiro ser vivo a orbitar o planeta Terra?','Laika','Belka','Sally', 'Strelka',1);
+insert into quiz values (27,'Que planeta no Sistema Solar tem o eixo de rotação inclinado 98º?','Urano','Vénus','Júpiter', 'Saturno',1);
+insert into quiz values (28,'Mimas, Dione e Titã são luas de que planeta?','Saturno','Júpiter','Neptuno', 'Urano',1);
+insert into quiz values (29,'Ceres é um planeta anão, entre que planetas órbita?','Marte e Júpiter','Saturno e Urano','Proxima Centauri a e Proxima Centauri b', 'Júpiter e Saturno',1);
+insert into quiz values (30,'Quantos planetas no Sistema Solar têm luas?','6','7','4', '5',1);
 -- insert into quiz values (31,'','','',' ', '');
 -- insert into quiz values (32,'','','',' ', '');
 -- insert into quiz values (33,'','','',' ', '');
--- server.py
+++ server.py
@@ -14,10 +14,10 @@ mysql = MySQL(app)
 
 mysql.init_app(app)
 
-nr_players=0
+nr_players = 0
 r= []
-waitingPlayer=0
-counter=0
+waitingPlayer = 0
+counter = 0
 winner = 0
 timePlayer1 = -1
 rightPlayer1 = -1
@@ -60,13 +60,14 @@ def getWinner():
 @app.route('/index',  methods=['POST', 'GET'] )
 def playerInformation():
 	global nr_players
+	global waitingPlayer
 	if request.method == "POST":
 		if nr_players<2:
-			nr_players=nr_players+1
-			code=nr_players
+			nr_players = nr_players+1
+			code = nr_players
 			insertPlayer(request.form.get('name'),request.form.get('course'),request.form.get('id'),request.form.get('mail'),0,0,0)
-			if nr_players==2:
-				waitingPlayer=1
+			if nr_players == 2:
+				waitingPlayer = 1
 			return jsonify(code)
 		return jsonify(0)
 	return jsonify(0)
@@ -104,7 +105,7 @@ def PlayerResult():
 			elif int(aux['code']) == 2:
 				timePlayer2= aux['time']
 				rightPlayer2=aux['correct']
-			update_results(aux['time'],aux['correct'],aux['total'],aux['id'])
+			update_results(aux['time'],aux['correct'],6,aux['id'])
 	return jsonify(0)
 
 
@@ -131,6 +132,26 @@ def StartArduino():
 	aux = winner
 	winner = 0 
 	return jsonify(aux)
+
+@app.route('/fresh', methods=['POST', 'GET'])
+def fresh():
+	global winner
+	global nr_players
+	global counter
+	global waitingPlayer
+	global timePlayer1 
+	global rightPlayer1 
+	global timePlayer2 
+	global rightPlayer2 
+	nr_players = 0
+	counter = 0
+	waitingPlayer = 0
+	timePlayer1 = -1
+	rightPlayer1 = -1
+	timePlayer2 = -1
+	rightPlayer2 = -1
+	winner = 0 
+	return jsonify(1)
 
 
 
