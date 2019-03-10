e3133d425363d11d1989e94441c538f72b469182
JoaoLuisGois <joao.luis.gois@gmail.com>
2019-03-10 17:07:11 +0000

    adicionei a função extra no programa do arduino

--- arduino/server.ino
+++ arduino/server.ino
@@ -14,7 +14,7 @@ const char* ssid = "DESKTOP-AN1JALD 0704";
 const char* password = "03Y,7n80";
 
 WiFiServer server(80);
- 
+
 void setup() {
   Serial.begin(9600);
   // Connect to WiFi network
@@ -29,8 +29,10 @@ void setup() {
     delay(50);
     Serial.print(".");
   }
-  Servo1.attach(servoPin); 
-  
+  Servo1.attach(servoPin);
+  Servo1.write(90); // Posição inicial servo
+  shake();
+
 
 }
 
@@ -39,27 +41,39 @@ void loop() {
  // Servo1.write(90);
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     HTTPClient http;  //Declare an object of class HTTPClient
- 
+
     http.begin("http://hackerschool.io:8080/start");  //Specify request destination
     int httpCode = http.GET(); //Send the request
     if (httpCode > 0) { //Check the returning code
- 
+
       String payload = http.getString();   //Get the request response payload
 
       //se ganhar o jogador 1 roda para um lado
        if(payload.toInt()==1){
         Servo1.write(0); // Make servo go to 0 degrees
         delay(1000);
-        Servo1.write(90); 
+        Servo1.write(90);
+        shake();
       }
       //se ganhar o jogador 2 roda para o lado contrario
       else if(payload.toInt()==2){
-        Servo1.write(180); 
+        Servo1.write(180);
         delay(2000);
         Servo1.write(90);
+        shake();
       }
     }
     http.end();   //Close connection
   }
-  delay(1000);    
+  delay(1000);
 }
+// Função para evitar que os doces fiquem presos.
+void shake() {
+  for (i=0; i <= 5; i++){
+    Servo1.write(80);
+    delay(100);
+    Servo1.write(100);
+    delay(100);
+    Servo1.write(90);
+    delay(100);
+  }
